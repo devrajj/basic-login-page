d804739ae7335669844d3cd29c1e07248bdb5bf8
Devraj Sen <devrajs@valuefy.com>
2022-04-17 23:45:38 +0530

    added code for live chatting through socket

--- server/models/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost:27017/userInformation");
-const usersSchema = new mongoose.Schema({
-  email: String,
-  name: String,
-  password: String
-})
-const User = new mongoose.model("User", usersSchema);
-module.exports = User;
--- server/server.js
+++ server/server.js
@@ -2,10 +2,33 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
-const User = require('../server/models/user.js');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 const salt = bcrypt.genSaltSync(saltRounds);
+const connection = require('./connection.js');
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+io.on('connection', (socket) => {
+
+  socket.on('joining msg', (username) => {
+    var name = username;
+    io.emit('chat message', `---${name} joined the chat---`);
+  });
+
+  socket.on('disconnect', (username) => {
+    var name = username;
+    io.emit('chat message', `---${name} left the chat---`);
+
+  });
+  socket.on('chat message', async (msg) => {
+    let username = msg ? msg.split(':')[0].trim() : '';
+    let text = msg ? msg.split(':')[1].trim() : '';
+    let userCreated = await createMessagingLogs(text, username);
+    socket.broadcast.emit('chat message', msg);
+  });
+});
 app.engine('html', require('ejs').renderFile);
 app.use(express.static(path.join(__dirname, '/../client')));
 app.use(bodyParser.json());
@@ -21,22 +44,33 @@ app.post('/login', (req, res) => {
   let password = req.body.psw;
   let hashPassword = '';
   let userName;
-  User.findOne({ email: email }, (err, document) => {
-    if (err) {
-      res.render(path.join(__dirname, '/../client/html/loginpage.html'), { message: 'Error in connection. Please contact support team' });
-    }
-    hashPassword = document && document.password ? document.password : null;
-    userName = document && document.name ? document.name.toUpperCase() : null;
-    if (hashPassword) {
-      if (bcrypt.compareSync(password, hashPassword)) {
-        res.render(path.join(__dirname, '/../client/html/loginpage.html'), { message: '', userName: userName });
-      } else {
-        res.render(path.join(__dirname, '/../client/html/loginpage.html'), { message: 'Password doesnot match. Please enter correct password' });
+  return new Promise((resolve, reject) => {
+    let sql = 'select * from chatapplication.user where email=?';
+    connection.query(sql, [email], function (err, result) {
+      if (err) {
+        res.render(path.join(__dirname, '/../client/html/loginpage.html'), { message: 'Error in connection. Please contact support team', userName: '', messageArray: [] });
       }
-    } else {
-      res.render(path.join(__dirname, '/../client/html/loginpage.html'), { message: 'User doesnot exist with the given mail' });
-    }
-  });
+      let document = result && result[0] ? result[0] : {};
+      hashPassword = document && document.password ? document.password : null;
+      userName = document && document.name ? document.name.toUpperCase() : null;
+      if (hashPassword) {
+        if (bcrypt.compareSync(password, hashPassword)) {
+          let sql = 'select * from chatapplication.message_history';
+          connection.query(sql, [email], function (err, result) {
+            if (err) {
+              res.render(path.join(__dirname, '/../client/html/loginpage.html'), { message: 'Error in connection. Please contact support team', userName: '', messageArray: [] });
+            } else {
+              res.render(path.join(__dirname, '/../client/html/loginpage.html'), { message: '', userName: userName, messageArray: result });
+            }
+          })
+        } else {
+          res.render(path.join(__dirname, '/../client/html/loginpage.html'), { message: 'Password doesnot match. Please enter correct password', userName: '', messageArray: [] });
+        }
+      } else {
+        res.render(path.join(__dirname, '/../client/html/loginpage.html'), { message: 'User doesnot exist with the given mail', userName: '', messageArray: [] });
+      }
+    });
+  })
 });
 app.post('/registerNewUser', async (req, res) => {
   let email = req.body.email;
@@ -45,45 +79,58 @@ app.post('/registerNewUser', async (req, res) => {
   userName = userName.toUpperCase();
   const hashPassword = bcrypt.hashSync(password, salt);
   let foundEmail = await checkIfEmailExists(email);
-  if (foundEmail) {
+  if (foundEmail.length) {
     res.render(path.join(__dirname, '/../client/html/signupSuccess.html'), { message: 'User Already exists with the given email. Please signup with a new email' });
   } else {
     let newusercreated = await createNewUser(email, hashPassword, userName);
-    if (newusercreated && newusercreated.success) {
+    if (newusercreated) {
       res.render(path.join(__dirname, '/../client/html/signupSuccess.html'), { message: '' });
     } else {
       res.render(path.join(__dirname, '/../client/html/signupSuccess.html'), { message: 'Error while registering user' });
     }
   }
 });
-app.listen(3000, () => {
+
+server.listen(3000, () => {
   console.log('Server started at http://localhost:3000');
 })
 
 function checkIfEmailExists(email) {
   return new Promise((resolve, reject) => {
-    User.findOne({ email: email }, (err, document) => {
+    let sql = 'select * from chatapplication.user where email=?';
+    connection.query(sql, [email], function (err, result) {
       if (err) {
         return reject(err);
+      } else {
+        return resolve(result);
       }
-      return resolve(document);
     });
-  })
+  });
 }
 
 function createNewUser(email, hashPassword, userName) {
   return new Promise((resolve, reject) => {
-    const newUser = new User({
-      email: email,
-      password: hashPassword,
-      name: userName
-    });
-    newUser.save((err) => {
+    let sql = 'INSERT INTO chatapplication.user (email,password,name) values (?,?,?)';
+    connection.query(sql, [email, hashPassword, userName], function (err, result) {
       if (err) {
         return reject(err);
       } else {
-        return resolve({ success: true });
+        return resolve(result);
       }
-    })
+    });
   })
 }
+
+function createMessagingLogs(text, username) {
+  return new Promise((resolve, reject) => {
+    let sql = 'INSERT INTO chatapplication.message_history (text,username) values (?,?)';
+    connection.query(sql, [text, username], function (err, result) {
+      if (err) {
+        return reject(err);
+      } else {
+        return resolve(result);
+      }
+    });
+  })
+}
+
